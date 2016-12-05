dda06550b56e107832fcf21f246ab73926979a6a
Giancarlo <gfabrizi@gmail.com>
2016-12-05 14:01:11 +0100

    adds image logo to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:welcome_msg, :description, :start_date, :end_date, :note, :name)
+      params.require(:event).permit(:welcome_msg, :description, :start_date, :end_date, :note, :name, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205105634) do
+ActiveRecord::Schema.define(version: 20161205123441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,13 @@ ActiveRecord::Schema.define(version: 20161205105634) do
     t.date     "end_date"
     t.string   "note"
     t.string   "name"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
